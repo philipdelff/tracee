b58af6bc970a8d4b678255f348e4e8754116ece5
Philip Delff <philip@delff.dk>
2023-03-16 10:51:24 -0400

    stop if no plot given to ggwrite

--- R/ggwrite.R
+++ R/ggwrite.R
@@ -71,7 +71,9 @@ ggwrite <- function(plot, file, script, time, canvas="standard", formats,
     
 ### Section end: Dummy variables, only not to get NOTE's in pacakge checks
 
-    
+    if(missing(plot) || !exists(plot)){
+        stop("An existing plot must be passed as the plot argument.")
+    }
 
     if(useNames && length(plot)==1) warning("useNames is ignored because plot is of length 1.")
 
