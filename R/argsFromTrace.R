42bf65997e53af8c593a061bcc76b85f3b6f32bf
Philip Delff <philip@delff.dk>
2026-01-16 12:13:02 -0500

    argsfromtrace

---
+++ R/argsFromTrace.R
@@ -0,0 +1,13 @@
+argsFromTrace <- function(fun, x, attr = "args") {
+
+  if (!inherits(x, "trace")) return(list())
+
+  defs <- attr(x, defaults_attr)
+  if (!is.list(defs)) return(list())
+
+  arg_names <- setdiff(names(formals(fun)), "x")
+  mc <- match.call(definition = fun, expand.dots = FALSE)
+
+  ## only defaults for args that were not supplied
+  defs[names(defs) %in% arg_names & !(names(defs) %in% names(mc))]
+}
