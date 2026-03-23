647e77ef61ad356b721dd73c1b3eaa6c4d76e681
delff <Philip_Delff@vrtx.com>
2026-03-23 16:42:56 -0400

    including filePathSimple until NMdata 0.2.4

---
+++ R/filePathSimple.R
@@ -0,0 +1,43 @@
+##' Clean and standardize file system paths
+##'
+##' @description Use this to tidy up paths. Combines pieces of a path like
+##'     file.path(). The function is intended to return a canonical
+##'     path format, i.e. paths that can be compared by simple string
+##'     comparison. Redundant /'s removed. normalizePath is used to
+##'     possibly shorten path.
+##' @param ... additional arguments passed to file.path().
+##' @return A (character) file path
+##' @family FileSystem
+##' @keywords internal
+
+#### Borrowed from NMdata. Needed until NMdata 0.2.4
+
+filePathSimple <- function(...){
+
+### drop empty
+    dots <- list(...)
+    dots <- dots[!sapply(dots,is.null)]
+    
+    fpath <- do.call(file.path,dots)
+    ## get rid of heading and tailing white spaces
+    fpath <- trimws(fpath)
+    ## convert double \\ into /
+    fpath <- gsub(pattern="\\\\",replacement="/",x=fpath)
+    ## removing redundant /'s that make it harder to compare paths. Not in beginning of paths, because they are network paths.
+    slashes.lead <- sub("^(/*).*","\\1",fpath)
+    after.slashes <- sub("^/+","",fpath)
+    after.slashes2 <-  gsub(pattern="//",replacement="/",after.slashes)
+    fpath <- paste0(slashes.lead,after.slashes2)
+##    fpath <- gsub(pattern="(?<!^)/+",replacement="/",fpath)
+    ## a dir path should not end in a / (which again, makes comparissons more complicate)
+    fpath <- gsub(pattern="/+$",replacement="",x=fpath)
+    
+    
+    ## Denote windows drives with capital letter
+    fpath <- sub("^([a-z]):/","\\U\\1:/",fpath,perl=TRUE)
+    is.win.abs <- grepl("^[a-z]:/",fpath,ignore.case=TRUE,perl=TRUE)
+    if(any(is.win.abs)){
+        fpath[is.win.abs] <- normalizePath(fpath[is.win.abs],mustWork=FALSE,winslash="/")
+    }
+    return(fpath)
+}
---
+++ devel/lwrite_v1.R
@@ -0,0 +1,41 @@
+## write roxygen documentation 
+lwrite <- function(list,model,dir=".",structure,...){
+    
+    if(missing(model)) model <- NULL
+
+    if(is.list(model)) {
+        model <- model$label
+    }
+
+### how about if fun.path is a string, like "model/model-file"?
+### Shouldn't we run pathStruct on it?
+    if(missing(structure)) structure <- NULL
+
+    ## if( !is.null(model) ){
+    ##     fun.path <- pathStruct(structure=structure)
+    ##     dir.model <- fun.path("dummy",dir,model) |> dirname() 
+    ## } else {
+    ##     fun.path(name,dir,model) file.path(dir,name)
+    ## } 
+
+    fun.path <- pathStruct(structure=structure)
+
+    dir.model <- fun.path("dummy",dir,model) |> dirname()
+    if(!file.exists(dir.model)) dir.create(dir.model)
+    
+    ## Collect plots in one list. When should this be done?
+    plots <- list[sapply(list,is.gg)]
+    notplots <- list[!sapply(list,is.gg)]
+    list <- c(notplots,plots=list(plots))
+
+
+    names.out <- names(list)
+    res <- lapply(1:length(list),function(n){
+        file.out <- fun.path(name=names.out[n],dir=dir,model=model)
+        writer(x=list[[n]],
+               file=file.out,
+               model=model,
+               ...)
+    })
+    return(invisible(res))
+}
