42d8b017f59b60e68eb0fe7a5a2113f5041b01bb
delff <Philip_Delff@vrtx.com>
2026-05-25 19:19:53 -0400

    minor

--- R/ftwrite.R
+++ R/ftwrite.R
@@ -19,12 +19,13 @@
 ##'     powerpoint.
 ##' @param quiet Default is false but use TRUE to suppress messages
 ##'     about what was saved.
+##' @param bg not used. Included for backward compatibility.
 ##' @param ... Arguments passed to stampFlextab.
 ##' @import flextable
 ##' @importFrom NMdata fnExtension
 ##' @export
 
-ftwrite <- function(ft,file,formats,save=TRUE,show=!save,quiet=FALSE,script,time,model,format.stamp,fun.path,subdir=NULL){
+ftwrite <- function(ft,file,formats,save=TRUE,show=!save,quiet=FALSE,script,time,model,format.stamp,fun.path,subdir=NULL,bg){
   
   
   use.names <- TRUE
@@ -55,26 +56,6 @@ ftwrite <- function(ft,file,formats,save=TRUE,show=!save,quiet=FALSE,script,time
 
   names.all <- names.els
 
-  if(F){
-    subdir <- NULL
-    if(lists.as.subdirs){
-      if(use.names) {
-        names.all <- names.els
-      } else {
-        ## names.all <- paste(names.els,sep="_")
-        ## names.all <- padZeros(paste(1:length(names.els)))
-        names.all <- names.els
-      }
-      subdir <- file
-    } else {
-      if(use.names) {
-        ### if no names provided, they will all get the same name(?) 
-        names.all <- rep(file,length(ft))
-      } else {
-        names.all <- paste(file,names.els,sep="_")
-      }
-    }
-  }
 
   if(missing(format.stamp)) format.stamp <- NULL
 
